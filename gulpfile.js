ce33a4d9cc63aaa84f75cab4dd3461b61091429d
Jasper Meijaard <github@mojominds.com>
2016-01-17 17:32:23 +0100

    Uniform names

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('browsersync-run', () => {
   browsersync.init(config.browsersync.options);
 });
 
-gulp.task('js-transpile', () => {
+gulp.task('js-processor', () => {
   del(config.js.del);
   return gulp.src(config.js.src)
     .pipe(sourcemaps.init())
@@ -33,26 +33,26 @@ gulp.task('js-transpile', () => {
     .pipe(browsersync.stream());
 });
 
-gulp.task('js-watch', ['js-transpile'], () => {
+gulp.task('js-watch', ['js-processor'], () => {
   return watch([config.js.src], () => {
-    gulp.start(['js-transpile']);
+    gulp.start(['js-processor']);
   });
 });
 
-gulp.task('html-compile', () => {
+gulp.task('html-processor', () => {
   del(config.html.del);
   return gulp.src(config.html.src)
     .pipe(gulp.dest(config.html.dist))
     .pipe(browsersync.stream());
 });
 
-gulp.task('html-watch', ['html-compile'], () => {
+gulp.task('html-watch', ['html-processor'], () => {
   return watch([config.html.src], () => {
-    gulp.start(['html-compile']);
+    gulp.start(['html-processor']);
   });
 });
 
-gulp.task('sass-compile', () => {
+gulp.task('sass-processor', () => {
   del(config.sass.del);
   return gulp.src(config.sass.src)
     .pipe(sourcemaps.init())
@@ -62,8 +62,8 @@ gulp.task('sass-compile', () => {
     .pipe(browsersync.stream());
 });
 
-gulp.task('sass-watch', ['sass-compile'], () => {
+gulp.task('sass-watch', ['sass-processor'], () => {
   return watch([config.sass.src], () => {
-    gulp.start('sass-compile');
+    gulp.start('sass-processor');
   });
 });
